7fe93593a7791f057f33e044bb69512eec803138
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-07-11 19:44:21 -0400

    BranchSums

---
+++ practice/BranchSums.java
@@ -0,0 +1,43 @@
+package DSandAlgorithmsPractice.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BranchSums {
+
+    public static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        BinaryTree(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static List<Integer> branchSums(BinaryTree root) {
+        List<Integer> sums = new ArrayList<Integer>();
+        calculateBranchSums(root,0,sums);
+        return sums;
+    }
+
+    public static void calculateBranchSums(BinaryTree node, int runSum, List<Integer> sums){
+        if(node == null) {
+            return;
+        }
+
+        int newRunSum = runSum + node.value;
+
+        if(node.left == null && node.right == null){
+            sums.add(newRunSum);
+            return;
+        }
+
+        calculateBranchSums(node.left, newRunSum, sums);
+
+        calculateBranchSums(node.right, newRunSum, sums);
+
+    }
+}
