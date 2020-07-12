44471ece3ddf98cc20d5391b6e8c34a780442e08
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-07-12 12:11:10 -0400

    nodeDepthsBST

---
+++ practice/NodeDepths.java
@@ -0,0 +1,29 @@
+package DSandAlgorithmsPractice.practice;
+
+public class NodeDepths {
+
+    public static int nodeDepths(BinaryTree root) {
+        return nodeDepthsRecursive(root, 0);
+    }
+
+
+    private static int nodeDepthsRecursive(BinaryTree root, int depth){
+        if(root == null){
+            return 0;
+        }
+        return depth + nodeDepthsRecursive(root.left, depth + 1) + nodeDepthsRecursive(root.right, depth + 1);
+
+    }
+
+    static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+}
