120a63bf3a5c0c725644c2fb11b4327ac469e9dc
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-07-30 00:10:21 -0400

    NumberOfWaysToMakeChange

---
+++ practice/medium/NumberOfWaysToMakeChange.java
@@ -0,0 +1,19 @@
+package DSandAlgorithmsPractice.practice.medium;
+
+public class NumberOfWaysToMakeChange {
+    //Time = o(nd) || space = 0(n) d is number of denominations
+    public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+        int[] ways = new int[n +1];
+        ways[0] = 1;
+        for(int denom : denoms){
+            for(int amount = 1; amount < n +1 ; amount ++){
+                if(denom <= amount){
+                    ways[amount] = ways[amount] + ways[amount - denom];
+                }
+            }
+        }
+        //ways[amount] = ways[amount] + ways[amount - denom]
+        return ways[n];
+    }
+
+}
