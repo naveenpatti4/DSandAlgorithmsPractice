54874f27c752d9271e16dbe05abc6c5a4dfc5051
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-08-14 00:22:31 -0400

    mergeSortedArraysInPlace

--- practice/sorting/Merge2SortedArrays.java
+++ practice/sorting/Merge2SortedArrays.java
@@ -7,10 +7,15 @@ import java.util.Arrays;
 public class Merge2SortedArrays {
 
    public static void main(String[] args) {
-      int[] arr1 = {1, 3, 4, 5};
-      int[] arr2 = {2, 4, 6, 8};
-      int[] strings = mergeSortedArrays(arr1, arr2);
-      Arrays.stream(strings).forEach(System.out::println);
+//      int[] arr1 = {1, 3, 4, 5};
+//      int[] arr2 = {2, 4, 6, 8};
+//      int[] strings = mergeSortedArrays(arr1, arr2);
+//      Arrays.stream(strings).forEach(System.out::println);
+
+      int[] arr3 = {1, 3, 4, 5};
+      int[] arr4 = {2, 4, 6, 8, 0, 0, 0,0};
+      int[] arr = mergeSortedArraysInPlace(arr3, arr4);
+      Arrays.stream(arr).forEach(System.out::println);
    }
 
    //O(n) time
@@ -42,6 +47,36 @@ public class Merge2SortedArrays {
       }
 
       return mergedArray;
+   }
+
+
+   //O(n) time
+   public static int[] mergeSortedArraysInPlace(int[] arr1, int[] arr2) {
+
+      int first = arr1.length-1, second = arr1.length-1, mergingArrayLength = arr2.length-1;
+      while (first >=0 && second >=0) {
+         if (arr1[first] > arr2[second]) {
+            arr2[mergingArrayLength] = arr1[first];
+            first--; mergingArrayLength--;
+         } else {
+            arr2[mergingArrayLength] = arr2[second];
+            second--; mergingArrayLength--;
+         }
+      }
+
+      while (first >= 0) {
+         arr2[mergingArrayLength] = arr1[first];
+         first--;
+         mergingArrayLength--;
+      }
+
+      while (second >=0 ) {
+         arr2[mergingArrayLength] = arr2[second];
+         second--;
+         mergingArrayLength--;
+      }
+
+      return arr2;
    }
 
 
