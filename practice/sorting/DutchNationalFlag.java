a4acb7af04c561e518bf3166b1bd35e7e1822ceb
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-08-14 12:02:03 -0400

    mergeSortedArraysInPlaceWithIntegeres

--- practice/sorting/DutchNationalFlag.java
+++ practice/sorting/DutchNationalFlag.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 
 public class DutchNationalFlag {
    public static void main(String[] args) {
-      char[] chars = {'R','B','G','G','R','B','G'};
-      char[] result = dutchNational(chars);
-      Arrays.stream(new char[][]{result}).forEach(System.out::println);
+//      char[] chars = {'R','B','G','G','R','B','G'};
+//      char[] result = dutchNational(chars);
+//      Arrays.stream(new char[][]{result}).forEach(System.out::println);
+
+      int[] nums = {2,0,2,1,1,0, 0, 2, 1, 0, 0 ,0 ,2, 0, 1};
+      int[] res = sortColors(nums);
+      Arrays.stream(res).forEach(System.out::println);
    }
 
    private static char[] dutchNational(char[] chars) {
@@ -33,4 +37,34 @@ public class DutchNationalFlag {
       chars[j] = chars[i];
       chars[i] = temp;
    }
+
+
+   public static int[] sortColors(int[] nums) {
+
+      int i = 0;
+      int lP = 0;
+      int rP = nums.length-1;
+
+      while( i <= rP ){
+         if(nums[i] == 0) {
+            swap(nums, lP, i);
+            i++; lP ++;
+         } else if(nums[i] == 2){
+            swap(nums, rP, i);
+            rP --;
+         } else if(nums[i] == 1){
+            i++;
+         }
+      }
+
+      return nums;
+
+   }
+
+   private static void swap(int[] nums, int i , int j){
+      int temp = nums[j];
+      nums[j] = nums[i];
+      nums[i] = temp;
+   }
+
 }
