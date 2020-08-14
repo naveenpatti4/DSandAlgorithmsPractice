72f9461e6e210d9f71f405118d656ea4ed457576
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-08-13 22:47:33 -0400

    DutchNationalFlag.java

--- practice/sorting/Merge2SortedArrays.java
+++ practice/sorting/Merge2SortedArrays.java
@@ -2,7 +2,9 @@ package DSandAlgorithmsPractice.practice.sorting;
 
 import java.util.Arrays;
 
-public class MergeSortedArrays {
+//Merge Two Sorted Arrays
+
+public class Merge2SortedArrays {
 
    public static void main(String[] args) {
       int[] arr1 = {1, 3, 4, 5};
@@ -41,6 +43,9 @@ public class MergeSortedArrays {
 
       return mergedArray;
    }
+
+
+
 
 }
 
