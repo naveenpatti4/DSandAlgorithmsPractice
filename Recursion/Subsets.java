f07a5aee7242eb9afa98ca87efa26b2c4b9da543
Naveen Patti <30995561+naveenpatti4@users.noreply.github.com>
2020-08-19 22:45:26 -0400

    Subsets with strings

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -1,6 +1,9 @@
 package DSandAlgorithmsPractice.Recursion;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,8 +11,11 @@ public class Subsets {
 
    public static void main(String[] args) {
       int[] array2 = new int[]{1,3,2};
-      List<List<Integer>> res = subsets(array2);
-      System.out.println("res :: --> " + res);
+//      List<List<Integer>> res = subsets(array2);
+//      System.out.println("res :: --> " + res);
+
+      String[] op = generate_all_subsets("xy");
+      Arrays.stream(op).forEach(System.out::println);
 
    }
 
@@ -41,4 +47,35 @@ public class Subsets {
       }
 
    }
+
+   static String[] generate_all_subsets(String s) {
+
+      List<String> strings = new ArrayList<>();
+
+      strHelper(s, 0, new StringBuilder(),strings);
+
+      return strings.toArray(new String[0]);
+   }
+
+
+   private static void strHelper(String s, int idx, StringBuilder slate,  List<String> strings ){
+
+      //base case
+      if (idx == s.length()){
+         strings.add(slate.toString());
+         return;
+      }
+      //recursive case
+      else{
+         //exclude
+         strHelper(s, idx+1, slate, strings);
+
+         //include
+         slate.append(s.charAt(idx));
+         strHelper(s, idx+1, slate, strings);
+         slate.deleteCharAt(slate.length()-1);
+      }
+
+   }
+
 }
